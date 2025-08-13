a6c535b641cd4f05b6c03961afa5c18c0b8816e6
Abu Hurayra <abuhuraira3912l@gmail.com>
2025-08-13 11:33:22 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Abuhurara-website
+
---
+++ main.js
@@ -0,0 +1,28 @@
+ // Change navbar color on scroll
+window.addEventListener('scroll', function () {
+  const nav = document.querySelector('.navigation');
+  if (window.scrollY > 0) {
+    nav.classList.add('window-scroll');
+  } else {
+    nav.classList.remove('window-scroll');
+  }
+});
+
+// Show and hide question and answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach((faq) => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+    changeIcon(faq);
+  });
+});
+
+
+
+  // year 
+  document.getElementById("year").textContent = new Date().getFullYear();
+
+  let emlement = document.querySelectorAll('section');
+  emlement.forEach(sec => {
+    
+  });
